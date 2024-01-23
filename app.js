c63dab95676cc8dc2892b31fa79a53cc5c803028
DevAlteria <147919439+DevAlteria@users.noreply.github.com>
2024-01-23 10:08:16 +0000

    Add webpack configuration and dependencies

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const fs = require("fs");
+const config = require("./webpack.config.js");
+const compiler = webpack(config);
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
+app.use(
+    webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+    })
+    );
+    
+app.use("/assets", express.static("assets"));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/view.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./public",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+};
