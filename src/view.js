6b4b0145d2474e37edef78150b2f22c5a223860a
DevAlteria <147919439+DevAlteria@users.noreply.github.com>
2024-01-23 12:39:51 +0000

    Update view.js rotation based on last_data values

--- src/view.js
+++ src/view.js
@@ -102,8 +102,6 @@ function onConnectionLost(responseObject) {
 client.onMessageArrived = onMessageArrived;
 function onMessageArrived(message) {
 	data = JSON.parse(message.payloadString);
-	console.log(data.time);
-	console.log(data.roll);
 	if (data.time > last_data.time) {
 		last_data = data;
 	}
@@ -291,8 +289,8 @@ import * as THREE from 'three';
 				const time = performance.now() * 0.001;
 
 				mesh.position.y = Math.sin( time ) * 20 + 5;
-				mesh.rotation.x = last_data.yaw;
-				mesh.rotation.y = last_data.pitch;
+				mesh.rotation.x = last_data.pitch;
+				mesh.rotation.y = last_data.yaw;
 				mesh.rotation.z = last_data.roll;
 
 				water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
