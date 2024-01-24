dfa9953558a33501521f328f2301470ed7049d56
DevAlteria <147919439+DevAlteria@users.noreply.github.com>
2024-01-24 09:57:10 +0000

    Remove unnecessary code and update rotation logic

--- src/view.js
+++ src/view.js
@@ -1,86 +1,3 @@
-/*
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { Water } from 'three/examples/jsm/objects/Water.js';
-import { Sky } from 'three/examples/jsm/objects/Sky.js';
-
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.5;
-document.body.appendChild( renderer.domElement );
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-const controls = new OrbitControls( camera, renderer.domElement );
-
-const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
-
-const water = new Water(
-	waterGeometry,
-	{
-		textureWidth: 512,
-		textureHeight: 512,
-		waterNormals: new THREE.TextureLoader().load( 'assets/textures/waternormals.jpg', function ( texture ) {
-
-			texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-
-		} ),
-		sunDirection: new THREE.Vector3(),
-		sunColor: 0xffffff,
-		waterColor: 0x001e0f,
-		distortionScale: 3.7,
-		fog: scene.fog !== undefined
-	}
-);
-
-water.rotation.x = - Math.PI / 2;
-
-scene.add( water );
-
-const sky = new Sky();
-sky.scale.setScalar( 10000 );
-scene.add( sky );
-
-const skyUniforms = sky.material.uniforms;
-
-skyUniforms[ 'turbidity' ].value = 10;
-skyUniforms[ 'rayleigh' ].value = 2;
-skyUniforms[ 'mieCoefficient' ].value = 0.005;
-skyUniforms[ 'mieDirectionalG' ].value = 0.8;
-
-const parameters = {
-	elevation: 2,
-	azimuth: 180
-};
-
-const pmremGenerator = new THREE.PMREMGenerator( renderer );
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
-
-	renderer.render( scene, camera );
-}
-
-animate();
-*/
-
 var data;
 var last_data = {};
 
@@ -328,7 +245,7 @@ function animate() {
 
 function render() {
 
-	if (boey !== undefined && last_data.roll !== undefined) {
+	if (boey !== undefined && last_data.time) {
 		console.log(last_data);
 		boey.rotation.x = +1.57 + last_data.roll;
 		boey.rotation.y = last_data.pitch;
